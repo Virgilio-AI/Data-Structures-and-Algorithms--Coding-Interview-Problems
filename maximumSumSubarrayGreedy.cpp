f5f6202f9de009d795bbe3c569b5fe555ac6adad
Virgilio Murillo <virgiliomurilloochoa1@gmail.com>
2020-12-30 21:06:02 -0600

    leetCodeInit

--- maximumSumSubarrayGreedy.cpp
+++ maximumSumSubarrayGreedy.cpp
@@ -1,24 +1,25 @@
-#include<bits/stdc++.h>			//== CREATED BY ROCKHIGHT == -- 14-07-20 13:06 --
-using namespace std;			//{{ frequency array }}
-//===== VARIABLES GLOBALES === -> === PROTOTIPOS DE FUNCIONES =====
-long long maxSumSubarray(int a[],int n){
-long long sum=0,ans=a[1];
-	for(int i=1;i<=n;i++){
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1000;
+int a[N],n;
+int solve()
+{
+	int sum=0,ans=0;
+	for(int i=0;i<n;i++)
+	{
 		sum+=a[i];
-		if(sum>ans)
-			ans=sum;
-		if(sum<0)
-			sum=0;
-		}
-return ans;
+		ans=max(sum,ans);
+		sum = (sum<0) ? 0:sum;
+	}
+	return ans;
 }
-//=====================		MAIN 	===================================
-int main(){
-int n;cin>>n;
-int a[n];
-for(int i=1;i<=n;i++){int temp;cin>>temp;a[i]=temp;}
-for(int i=1;i<=n;i++){cout<<a[i]<<" ";}
-cout<<"\n"<<maxSumSubarray(a,n);
-return 0;
+int main()
+{
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	cout<<solve();
+	return 0;
 }
-//======== FUNCIONES ========
