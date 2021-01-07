3a2466c804464a3d22dc7748ab4d5bb479cc53d2
Virgilio Murillo <virgiliomurilloochoa1@gmail.com>
2021-01-07 13:47:37 -0600

    ignore exe files in .gitignore and add fill Algorithm

--- fillAlgorithms.cpp
+++ fillAlgorithms.cpp
@@ -1,68 +1,64 @@
 #include<bits/stdc++.h>
 using namespace std;
-unsigned short Rows,Columns;
-const unsigned short N = 100;
-char Matrix[N][N];
+const int N = 100;
 bool Visited[N][N]={false};
-const short XDirections[4] = {0,1,0,-1};
-const short YDirections[4] = {1,0,-1,0};
-void FillAlgorithm(int Column,int Row,unsigned int &Counter)
-{    
-    Visited[Column][Row]=true;    
-    for(int i=0;i<3;i++)
-    {
-        int XPosition=Column+XDirections[i];
-        int YPosition=Column+YDirections[i];
-        if(XPosition>-1 && YPosition>-1 && XPosition < Rows && YPosition<Columns && !Visited[XPosition][YPosition] && Matrix[XPosition][YPosition] == '.')
-        {            
-            Counter++;
-            FillAlgorithm(Column,Row,Counter);
-        }
-    }
-}
-void FinIslands()
+char Map[N][N];
+int Rows,Columns;
+int XDirections[4] = {0,1,0,-1};
+int YDirections[4] = {-1,0,1,0};
+void IterarteMap();
+void FillAlgorithm(int Rows,int Columns,int &Counter);
+
+int main()
 {
-    cout<<"hola1"<<endl;
-    unsigned int NumberOfIslands=0,MaxIsland=0;
+    cin>>Rows>>Columns;
     for(int i=0;i<Rows;i++)
     {
         for(int j=0;j<Columns;j++)
         {
-            if(Matrix[i][j]=='.' && Visited[i][j]==false)
-            {
-                NumberOfIslands++;                
-                unsigned int Counter=1;
-                FillAlgorithm(i,j,Counter);
-                
-                MaxIsland = max(MaxIsland,Counter);                
-                cout<<NumberOfIslands<<" "<<MaxIsland<<endl;
-            }
+            cin>>Map[i][j];
         }
     }
-    cout<<NumberOfIslands<<" "<<MaxIsland<<endl;
-    cout<<"hola"<<endl;
-}
-int main()
-{
-    cout<<"hola tghrfdhgern etnrthnhertnrt nrtnrht nrthnrht nfghnfgnnfhnfgnhgfjhjfghjfgjhfg jfgjfgh fgh jfghjhfgfgj"<<endl;
-    // cin>>Rows>>Columns;
-    // cout<<Rows<<Columns;
-    // for(int i=0;i<Rows;i++)
-    // {
-    //     for(int j=0;j<Columns;j++)
-    //     {
-    //         cin>>Matrix[i][j];
-    //     }
-    // }
-    // FinIslands();
+    IterarteMap();    
     return 0;
 }
+void IterarteMap()
+{
+    int MaxIsland=0,NumberOfIslands=0;
+    for(int i=0;i<Rows;i++)
+    {
+        for(int j=0;j<Columns;j++)
+        {            
+            if(!Visited[i][j] && Map[i][j] == '.')
+            {
+                int Counter=0;
+                NumberOfIslands++;                
+                FillAlgorithm(i,j,Counter);
+                MaxIsland = max(MaxIsland,Counter);
+            }
+        }
+    }    
+    cout<<"Maximum Island:"<<MaxIsland<<"\nNumber of Islands:"<<NumberOfIslands;
+}
+void FillAlgorithm(int Row,int Column,int &Counter)
+{
+    Visited[Row][Column]=true;
+    Counter++;
+    for(int i=0;i<4;i++)
+    {
+        int XDirection = Column + XDirections[i];
+        int YDirection = Row + YDirections[i];        
+        if(XDirection>=0 && YDirection >=0 && XDirection < Columns && YDirection < Rows && Map[YDirection][XDirection] == '.' && !Visited[YDirection][XDirection])
+        {
+            FillAlgorithm(YDirection,XDirection,Counter);
+        }
+    }
+}
 /*
-sample input:
-5 5
-. # . # #
-# . . # .
-# . # . .
-# . . # #
-. . # . .
+5 6
+. # . # # .
+# . . # . .
+# . # . . #
+# . . # # .
+. . # . . .
 */
