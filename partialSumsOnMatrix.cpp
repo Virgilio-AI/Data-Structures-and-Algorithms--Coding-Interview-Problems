e2163713298dbff4dc85382541c515443c2e5c96
Virgilio Murillo <virgiliomurilloochoa1@gmail.com>
2021-01-15 12:31:49 -0600

    partialSumsOnMatrix updated and commented statement of problem

--- partialSumsOnMatrix.cpp
+++ partialSumsOnMatrix.cpp
@@ -1,47 +1,67 @@
-#include<bits/stdc++.h>			//== CREATED BY ROCKHIGHT == -- 16-07-20 12:42 --
-using namespace std;			//{{ title }}
-//===== VARIABLES GLOBALES === -> === PROTOTIPOS DE FUNCIONES =====
-const int Nmax=1007;
-int arr[Nmax][Nmax],n,m;
-long sums[Nmax][Nmax];
-int SumQuery(int l1,int l2,int c1,int c2){
-	return (sums[l2][c2]-sums[l1-1][c2]-sums[l2][c1-1]+sums[l1-1][c1-1]);	
-}
-int ps(int n,int m){
-	int sum=0;
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=m;j++){			sum+=arr[i][j]; }
-	}
-	return sum;
-}
-void createPartialSumsArr(){
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=n;j++){
-			sums[i][j]=ps(i,j);
+/*
+problem statement:
+given a matrix of n rows and m columns, answer for each k query.
+find the sum of all the elements inside the square given the coordinates of the upper left corner (l1,c1)
+and the coordinates of the lower right coordinate(l2,c2)
+input example:
+3 3//the number of rows and columns
+1 2 3 //the input matrix
+4 5 6//the input matrix
+7 8 9//the input matri
+3//the number of queries
+1 1 2 2 //the queries
+2 2 3 3//the queries
+3 3 3 3 //the queries
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+const int N=100;
+int Rows,Columns;
+int Queries,Matrix[N][N],PartialSumsSubMatrix[N][N];
+int x,x2,y,y2;
+//the creation of the partial sum submatrix, the partial sum sub matrix is basicly defined as 
+//PartialSumsSubMatrix[i][j] = the sum of all the elements in the matrix from Row=1 and Column=1 
+//to Row = i and Column + j
+// so for example the ,matrix
+//1 2 
+//3 4 
+//has a partial sum submatrix of 
+//1 3
+//4 10
+void CreatePartialSumSubMatrix()
+{
+	for(int i=1;i<=Rows;i++)
+	{
+		for(int j=1;j<=Columns;j++)
+		{
+			PartialSumsSubMatrix[i][j] = Matrix[i][j] + PartialSumsSubMatrix[i-1][j] + PartialSumsSubMatrix[i][j-1] - PartialSumsSubMatrix[i-1][j-1];
 		}
 	}
 }
-void createPartialSumsArrFast(){
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=n;j++){
-			sums[i][j]=sums[i][j-1]+sums[i-1][j]+arr[i][j]-sums[i-1][j-1];		
+//just a very intuitive propertie of the partial sum submatrix lets us know that the sum
+//of all the elements in the  sub matrix x y, x2 y2 is this result
+int PartialSumSubMatrixQuerie()
+{
+	return PartialSumsSubMatrix[x2][y2] + PartialSumsSubMatrix[x-1][y-1] - PartialSumsSubMatrix[x-1][y2] - PartialSumsSubMatrix[x2][y-1];
+}
+int main()
+{
+	cin>>Rows>>Columns;
+	for(int i=1;i<=Rows;i++)
+	{
+		for(int j=1;j<=Columns;j++)
+		{
+			cin>>Matrix[i][j];
 		}
 	}
-}
-//=====================		MAIN 	===================================
-int main(){
-cin>>n;
-cin>>m;
-for(int i=1;i<=n;i++){
-	for(int j=1;j<=n;j++){
-		cin>>arr[i][j];
-	}
-}
-createPartialSumsArrFast();
-while(m--){
-	int l1,l2,c1,c2;
-	cin>>l1>>c1>>l2>>c2;
-	cout<<SumQuery(l1,l2,c1,c2)<<"\n";
-}
-return 0;
+	CreatePartialSumSubMatrix();
+	cin>>Queries;
+	while(Queries--)
+	{
+		cin>>x>>y>>x2>>y2;
+		cout<<PartialSumSubMatrixQuerie()<<endl;		
+	}	
+
+	return 0;
 }
