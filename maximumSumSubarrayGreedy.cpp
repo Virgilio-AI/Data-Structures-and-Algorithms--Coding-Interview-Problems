bbec1596ea83d860d1199c5cb881f8758306a964
Virgilio Murillo <virgiliomurilloochoa1@gmail.com>
2021-01-20 15:29:49 -0600

    maximumSumSubarrayGreedy.cpp comented and updated

--- maximumSumSubarrayGreedy.cpp
+++ maximumSumSubarrayGreedy.cpp
@@ -1,25 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N=1000;
-int a[N],n;
-int solve()
+/*
+problem statement:
+create a program that given an array of integers you can 
+find the maximum sum sub array in the the current array,
+example:
+in:
+6
+-20 5 3 10 -13 12
+out:
+18
+*/
+
+int n;
+int MaximumSumSubArray(int arr[])
 {
-	int sum=0,ans=0;
+	int ans=arr[0],sum=0;
+	//you iterate over all the elements in the list 
+	//and you acumulate a sum variable
+	//if the sum variable is less than cero you will 
+	//reset the value of sum to 0
 	for(int i=0;i<n;i++)
 	{
-		sum+=a[i];
-		ans=max(sum,ans);
-		sum = (sum<0) ? 0:sum;
+		sum+=arr[i];
+		if(sum<0) sum=0;
+		ans = max(ans,sum);
 	}
 	return ans;
 }
+
 int main()
 {
 	cin>>n;
+	int arr[n];
 	for(int i=0;i<n;i++)
 	{
-		cin>>a[i];
+		cin>>arr[i];
 	}
-	cout<<solve();
+	
+	cout<<MaximumSumSubArray(arr)<<endl;
 	return 0;
 }
+
