31d62afbc18ef3c95bedf5ed599d89197cb5749f
Virgilio Murillo <virgiliomurilloochoa1@gmail.com>
2021-01-24 20:00:44 -0600

    TheLongestConsecutiveSubArray.cpp commented

--- longestConsecutiveNumsSubarray.cpp
+++ longestConsecutiveNumsSubarray.cpp
@@ -1,37 +1,65 @@
-#include<bits/stdc++.h>			//== CREATED BY ROCKHIGHT == -- 14-07-20 15:30 --
-using namespace std;			//{{ title }}
-//===== VARIABLES GLOBALES === -> === PROTOTIPOS DE FUNCIONES =====
-const int Nmax = 100001;
-int a[Nmax],n;
-bool fr[Nmax];
-int longestConsecNumsSubarray(){
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 100;
+bool fr[N]={0};
+/*
+ * given an array of n integers find the longest consecutive number subarray,
+ * so a consecutive number subarray is a subarray that if we sort the numbers the given subarray is going to be consecutive
+ * for example 1 4 3 2 is a consecutive subarray and 1 4 2 is not because is missing the 3 to be consecutive
+ * examples:
+ * in:
+ * 5
+ * 1 4 3 2 25
+ * out: 4
+ *
+ * in:
+ * 4
+ * 1 3 2 5
+ * out:
+ * 3
+ */
+
+int LongestConsecutiveNumsSubArray(int n,int arr[N])
+{
+	// we will use the sliding window tecnique so we can solve the problem in n^2
+	// given the observation that 1 condition has to be acomplished to have a consecutive 
+	// sub array,the biggest element minus the smaller has to be equal to the difference between the right position
+	//and the left position(the length) of the sub array
+	// and other condition has to be that there is no element repeated in the sub array, thus we will have an array of frequences
 	int ans=0;
-	for(int left =1;left<=n;left++){
-		for(int i=1;i<=n;i++){
-			fr[i]=false;
-			int Min=a[left],Max=a[left];
-			for(int right=left;right<=n;right++){
-				if(fr[a[right]]==true){
-					break;					
-				}
-				fr[a[right]]= true;
-				Min=min(Min,a[right]);
-				Max=max(Max,a[right]);
-				if(right-left==Max-Min){
-					ans=max(ans,right-left+1);
-				}
+	for(int left = 0;left<n;left++)
+	{
+		for(int i=0;i<n;i++)
+		{
+			fr[arr[i]]=false;
+		}
+		int Min = arr[left],Max=arr[left];
+		for(int right = left;right<n;right++)
+		{
+			if(fr[arr[right] ] == true)
+			{
+				break;
+			}
+			fr[arr[right]]=true;
+			Min = min(Min,arr[right]);
+			Max = max(Max,arr[right]);
+			if(right-left == Max-Min)
+			{
+				ans = max(ans,right-left+1);
 			}
 		}
-		return ans;
 	}
+	return ans;
 }
-//=====================		MAIN 	===================================
-int main(){
-cin>>n;
-for(int i=1;i<=n;i++){
-	cin>>a[i];	
+int main()
+{
+	int arr[N],n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	cout<<LongestConsecutiveNumsSubArray(n,arr)<<endl;
+	return 0;
 }
-cout<<longestConsecNumsSubarray();
-return 0;
-}
-//======== FUNCIONES ========
+
